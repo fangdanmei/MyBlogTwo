20b5e2edfb6b11ba2ebd9f6882afa2b8d45d16e4
fangdanmei <fangdanmei@300.cn>
2018-10-15 16:23:21 +0800

    BlogType contain id typename orderNo

--- src/entity/BlogType.java
+++ src/entity/BlogType.java
@@ -1,5 +1,30 @@
 package entity;
 
-public class BlogType {
+import java.io.Serializable;
+
+public class BlogType implements Serializable {
+	private static final long serialVersionID=1L;
+	private Integer id;
+	private String typeName;
+	private Integer orderNo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	public Integer getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(Integer orderNo) {
+		this.orderNo = orderNo;
+	}
+	
 
 }
