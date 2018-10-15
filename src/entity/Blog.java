dbffc7c5a318afce3e2caaff63b91d216421fa75
fangdanmei <fangdanmei@300.cn>
2018-10-15 15:35:42 +0800

    初始化提交

---
+++ src/entity/Blog.java
@@ -0,0 +1,77 @@
+package entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Blog implements Serializable {
+ 
+	private static final long serialVersionUID=1L;
+	
+	private Integer id;
+	private String title;
+	private String summary;
+	private Date releaseDate;
+	private Integer clickHit;
+	private Integer replyHit;
+	private String content;
+	private BlogType blogType;
+	private String keyWord;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public Integer getClickHit() {
+		return clickHit;
+	}
+	public void setClickHit(Integer clickHit) {
+		this.clickHit = clickHit;
+	}
+	public Integer getReplyHit() {
+		return replyHit;
+	}
+	public void setReplyHit(Integer replyHit) {
+		this.replyHit = replyHit;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public BlogType getBlogType() {
+		return blogType;
+	}
+	public void setBlogType(BlogType blogType) {
+		this.blogType = blogType;
+	}
+	public String getKeyWord() {
+		return keyWord;
+	}
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+	
+	
+	
+ 
+}
